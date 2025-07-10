a673fe0245e7f9400e4a770a950c9f466fca2b0f
Daniel Gil <daniel2303@msn.com>
2025-07-10 19:56:37 +0100

    fix: change public IP allocation to Static for reliable output

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "public-ip-demo"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Basic"
 }
 resource "azurerm_network_security_group" "nsg" {
