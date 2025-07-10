faec88777e31ca3b7d2eed4928f8688673f92d8b
Daniel Gil <daniel2303@msn.com>
2025-07-10 18:57:57 +0100

    feat: Initial provisioning of a Linux VM on Azure with Terraform

---
+++ main.tf
@@ -0,0 +1,63 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "610d9954-4b5c-444b-8836-68dc4e171057"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-demo"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-demo"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-demo"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key_path)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource Group name"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region"
+  default     = "westeurope"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "VM admin username"
+}
+
+variable "ssh_public_key_path" {
+  type        = string
+  description = "Path to the SSH public key"
+}
