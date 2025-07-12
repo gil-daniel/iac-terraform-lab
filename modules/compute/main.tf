2ab8a4eef5e4eb33a46d0c0e0a2cd763c63a0aef
Daniel Gil <daniel2303@msn.com>
2025-07-12 21:01:31 +0100

    fix: remove file() call from ssh_public_key_content to prevent CI error

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -30,7 +30,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file(var.ssh_public_key_content)
+    public_key = public_key = var.ssh_public_key_content
   }
 
   os_disk {
